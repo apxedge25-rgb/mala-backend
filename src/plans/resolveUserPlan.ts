9f4d3cecedd20cca928bfaec94af66c1e0e47d16
apxedge25-rgb <apxedge25@gmail.com>
2025-12-18 20:57:02 +0530

    Create resolveUserPlan.ts

---
+++ src/plans/resolveUserPlan.ts
@@ -0,0 +1,14 @@
+// src/plans/resolveUserPlan.ts
+
+import { PLANS } from "./config";
+import { PlanConfig, PlanId } from "./types";
+
+export function resolveUserPlan(req: any): PlanConfig {
+  const headerPlan = req.headers["x-mala-plan"] as PlanId | undefined;
+
+  if (headerPlan && PLANS[headerPlan]) {
+    return PLANS[headerPlan];
+  }
+
+  return PLANS.FREE;
+}
